bd15ef437cdf1ea9ae3732826d5c5180abbdb975
Tsyganov Andrey <andrytsy@mail.ru>
2019-05-25 22:35:47 +0300

    beejee repository create

---
+++ src/components/main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UsersList from '../users-list'
+
+function Main() {
+  return (
+    <div>
+      <h1>React App</h1>
+      <UsersList />
+    </div>
+  );
+}
+
+export default Main;
